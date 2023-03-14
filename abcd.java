e142e2aba475f10733e092b7ab8cb51a2929b08c
eara22 <esmatara70@gmail.com>
2023-03-14 14:59:05 -0400

    edited

--- abcd.java
+++ abcd.java
@@ -1,5 +1,7 @@
 public class abcd {
     public static void main(String[] args) {
         System.out.println("Today is Tuesday");
+        System.out.println("today is march fourteenth");
+
     }
 }
